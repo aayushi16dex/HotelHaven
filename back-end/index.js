08c57a2d9c045d140c60edc48389004b0c98cfea
aayushi16dex <aayushi16dex@gmail.com>
2023-07-08 00:39:37 +0530

    HotelHaven

--- back-end/index.js
+++ back-end/index.js
@@ -20,6 +20,7 @@ app.use(cors({
 }));
 
 app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5173');
     res.setHeader('Access-Control-Allow-Origin', 'https://master--hotelhaven.netlify.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
