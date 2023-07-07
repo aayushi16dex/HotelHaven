f20a8592cd2e70e130a7e2211fa4c9b5d8aaafc6
aayushi16dex <aayushi16dex@gmail.com>
2023-07-08 00:48:34 +0530

    HotelHaven

--- back-end/index.js
+++ back-end/index.js
@@ -22,7 +22,10 @@ const allowedOrigins = [
   }));
   
   app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', allowedOrigins.join(', '));
+    const origin = req.headers.origin;
+    if (allowedOrigins.includes(origin)) {
+      res.setHeader('Access-Control-Allow-Origin', origin);
+    }
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
     next();
