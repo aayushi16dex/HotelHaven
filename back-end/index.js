c5b18592ae7e732c3414dea768b493910f4c3b02
aayushi16dex <aayushi16dex@gmail.com>
2023-07-09 11:27:56 +0530

    hotelhaven

--- back-end/index.js
+++ back-end/index.js
@@ -27,7 +27,7 @@ const allowedOrigins = [
       res.setHeader('Access-Control-Allow-Origin', origin);
     }
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
   });
 
